a4d79fa00db3b7955e44a1e2a6b0d95c7b42cee1
Petr Urbánek <514122@mail.muni.cz>
2022-03-17 19:36:38 +0100

    homework finished

--- app/src/main/kotlin/cz/muni/fi/pv256/hw4/MainActivity.kt
+++ app/src/main/kotlin/cz/muni/fi/pv256/hw4/MainActivity.kt
@@ -2,7 +2,10 @@ package cz.muni.fi.pv256.hw4
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,15 +17,21 @@ class MainActivity : AppCompatActivity() {
         // 1. init live data
         // 2. start observing for count values and update counter accordingly
         // 3. set onClickListeners for buttons
+        val mavm: MainActivityViewModel by viewModels()
+        val counter = findViewById<TextView>(R.id.counter)
+        val myObserver = Observer<Int> { newName ->
+            counter.text = newName.toString()
+        }
+        mavm.data.observe(this, myObserver)
 
         val decrementBtn = findViewById<Button>(R.id.decrement)
         decrementBtn.setOnClickListener {
-            // TODO
+            mavm.decrementCount()
         }
 
         val incrementBtn = findViewById<Button>(R.id.increment)
         incrementBtn.setOnClickListener {
-            // TODO
+            mavm.incrementCount()
         }
     }
 }
--- app/src/main/kotlin/cz/muni/fi/pv256/hw4/MainActivityViewModel.kt
+++ app/src/main/kotlin/cz/muni/fi/pv256/hw4/MainActivityViewModel.kt
@@ -2,34 +2,52 @@ package cz.muni.fi.pv256.hw4
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 
 // TODO use SavedStateHandle
-class MainActivityViewModel : ViewModel(), Counter {
+class MainActivityViewModel(savedStateHandle: SavedStateHandle) : ViewModel(), Counter {
     companion object {
         private const val KEY_COUNT = "key_count"
     }
 
     private val _count: MutableLiveData<Int> by lazy {
         // TODO initialize with saved value
-        MutableLiveData<Int>().apply { value = 0 }
+        MutableLiveData<Int>().apply {
+            value = if (savedStateHandle.contains(KEY_COUNT)) {
+                savedStateHandle.get(KEY_COUNT)
+            } else {
+                0
+            }
+        }
     }
 
     val data: LiveData<Int>
         get() = _count
 
     override fun incrementCount() {
-        TODO("Not yet implemented")
+        // TODO
+        _count.value?.let { a ->
+            _count.value = a + 1
+        }
+        saveCountState()
     }
 
     override fun decrementCount() {
-        TODO("Not yet implemented")
+        // TODO
+        _count.value?.let { a ->
+            _count.value = a - 1
+        }
+        saveCountState()
     }
 
     /**
      * Use this function for saving state
      */
     private fun saveCountState() {
-        TODO("Not yet implemented")
+        // TODO
+        val map = mapOf<String, Int>()
+        map.plus(Pair(KEY_COUNT, _count.value))
+        SavedStateHandle(map)
     }
 }
